3c886a6dd5605234c628fe7ff1c30f49a993d7ed
L1997i <Lil147@pitt.edu>
2020-04-17 12:50:15 +0800

    add some structs and funcs

--- 4_cachelab/csim.c
+++ 4_cachelab/csim.c
@@ -12,46 +12,113 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <getopt.h>
+#include <math.h>
+
+struct cache_line
+{
+    int valid;
+    __uint64_t tag;
+
+    struct cache_line *prev;
+    struct cache_line *next;
+};
+
+struct cache_set
+{
+    int E; // E cache lines in each of the cache sets
+    struct cache_line *head;
+    struct cache_line *tail;
+};
+
+struct cache_sim
+{
+    int verbose;
+    int s;
+    int E;
+    int b;
+    char *trace_file;
+
+    int hit_count;
+    int miss_count;
+    int eviction_count;
+
+    struct cache_set **cache;
+};
+
+void print_help(void);
+struct cache_sim *new_csim(void);
+struct cache_sim *cache_init(int argc, char *argv[]);
 
 int main(int argc, char *argv[])
 {
-    extern char *optarg;
+    struct cache_sim *csim;
 
-    int verbose = 0;
-    int s = 0;
-    int E = 0;
-    int b = 0;
-    char *fileName = NULL;
+    csim = cache_init(argc, argv);
+
+    if (csim->verbose)
+    {
+        printf("%s", csim->trace_file);
+    }
+
+    printSummary(csim->hit_count, csim->miss_count, csim->eviction_count);
+    return 0;
+}
+
+struct cache_sim *new_csim(void)
+{
+    struct cache_sim *csim = malloc(sizeof(struct cache_sim));
+    csim = calloc(1, sizeof(struct cache_sim)); // set all space in csim to zero
+    return csim;
+}
+
+struct cache_sim *cache_init(int argc, char *argv[])
+{
+    struct cache_sim *csim = new_csim();
+    extern char *optarg;
     char opt;
 
     while ((opt = getopt(argc, argv, "hvs:E:b:t:")) != -1)
     {
         switch (opt)
         {
+        case 'h':
+            print_help();
+            exit(1);
         case 'v':
-            verbose = 1;
+            csim->verbose = 1;
             break;
         case 's':
-            s = atoi(optarg);
+            csim->s = atoi(optarg);
         case 'E':
-            E = atoi(optarg);
+            csim->E = atoi(optarg);
             break;
         case 'b':
-            b = atoi(optarg);
+            csim->b = atoi(optarg);
             break;
         case 't':
-            fileName = optarg;
+            csim->trace_file = optarg;
             break;
-        case 'h':
-            printf("usage: hvs:E:b:t:");
-            exit(1);
         }
     }
-    if (verbose)
-    {
-        printf("%d %d %d %s\n", s, E, b, fileName);
-    }
 
-    printSummary(0, 0, 0);
-    return 0;
+    // csim->hit_count = 0;
+    // csim->miss_count = 0;
+    // csim->eviction_count = 0;
+
+    return csim;
+}
+
+void print_help(void)
+{
+    printf("Usage: ./csim [-hv] -s <num> -E <num> -b <num> -t <file>\n");
+    printf("Options:\n");
+    printf("  -h              Print this help message.\n");
+    printf("  -v              Optional verbose flag.\n");
+    printf("  -s <num>        Number of set index bits.\n");
+    printf("  -E <num>        Number of lines per set.\n");
+    printf("  -b <num>        Number of block offset bits.\n");
+    printf("  -t <tracefile>  Name of the valgrind trace to replay.\n\n");
+    printf("Examples:\n");
+    printf("  linux>  ./csim -s 4 -E 1 -b 4 -t traces/yi.trace\n");
+    printf("  linux>  ./csim -v -s 4 -E 1 -b 4 -t traces/yi.trace\n");
 }
